9ba93c369ad6c0c6651b3cc747e4cb6edd6cf002
Tianhui Michael Li <tianhuil@cs.princeton.edu>
2018-06-02 21:49:01 -0400

    asynchronous call

--- angular-tour-of-heroes/src/app/hero.service.ts
+++ angular-tour-of-heroes/src/app/hero.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from '@angular/core';
 
+import { Observable, of } from 'rxjs';
+
 import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
 
@@ -8,8 +10,8 @@ import { HEROES } from './mock-heroes';
 })
 export class HeroService {
 
-  getHeroes(): Hero[] {
-    return HEROES;
+  getHeroes(): Observable<Hero[]> {
+    return of(HEROES);
   }
 
   constructor() { }
