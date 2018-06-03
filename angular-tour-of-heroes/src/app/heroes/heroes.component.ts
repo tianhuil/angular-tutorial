6e4066c4a3bcd18f0fb2fe5b8fd2411a1ac149ee
Tianhui Michael Li <tianhuil@cs.princeton.edu>
2018-06-03 16:13:22 -0400

    separating out into a full app

--- angular-tour-of-heroes/src/app/heroes/heroes.component.ts
+++ angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -10,10 +10,6 @@ import { Hero } from '../hero';
 })
 export class HeroesComponent implements OnInit {
   heroes: Hero[];
-  selectedHero: Hero;
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-  }
 
   constructor(private heroService: HeroService) { }
 
