9534c95eb56ce7d8e5c177824830e7e376b617cf
Tianhui Michael Li <tianhuil@cs.princeton.edu>
2018-06-10 14:17:58 -0400

    First reactive form

---
+++ angular-reactive-forms/src/app/hero-detail/hero-detail.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
+
+@Component({
+  selector: 'app-hero-detail',
+  templateUrl: './hero-detail.component.html',
+  styleUrls: ['./hero-detail.component.css']
+})
+export class HeroDetailComponent implements OnInit {
+  name = new FormControl();
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
