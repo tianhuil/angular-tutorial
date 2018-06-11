12e6d5f911b6389c401cf5cef21758560423c81d
Tianhui Michael Li <tianhuil@cs.princeton.edu>
2018-06-11 06:22:01 -0400

    initial setup

---
+++ angular-dynamic-forms/src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule } from '@angular/forms';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { DynamicFormComponent } from './dynamic-form/dynamic-form.component';
+import { DynamicFormQuestionComponent } from './dynamic-form-question/dynamic-form-question.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    DynamicFormComponent,
+    DynamicFormQuestionComponent,
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
